6474ee7cd9c9b1489164479a74d0bb6ccb11cfa5
kimgiraffe <kimgiraffe@naver.com>
2022-12-29 18:32:38 +0900

    함수

--- 221229/hw3.c
+++ 221229/hw3.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+int GCD(int n1,  int n2);
+
+void main(){
+    int num1 = 0, num2 = 0;
+    printf("input num1 : ");
+    scanf("%d", &num1);
+    printf("input num2 : ");
+    scanf("%d", &num2);
+    printf("GCD of num1 and num2 is %d\n", GCD(num1, num2));
+}
+
 int GCD(int n1, int n2)
 {
     int MAX = n2, tmp;
@@ -13,13 +24,4 @@ int GCD(int n1, int n2)
         n1 = tmp;
     }
     return n2;
-}
-
-void main(){
-    int num1 = 0, num2 = 0;
-    printf("input num1 : ");
-    scanf("%d", &num1);
-    printf("input num2 : ");
-    scanf("%d", &num2);
-    printf("GCD of num1 and num2 is %d\n", GCD(num1, num2));
 }
