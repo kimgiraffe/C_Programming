ea02e80a2a7ecc19053ba94663a8124f0458b2a3
kimgiraffe <kimgiraffe@naver.com>
2022-12-29 15:16:22 +0900

    함수

--- 221229/ex2.c
+++ 221229/ex2.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
+#define _USD 1195.50
+#define _EUR 1245.89
+#define _JPY 10.1949
 
-double toUSD(double KRW){
-    return KRW / 1195.50;
-}
-
-double toEUR(double KRW){
-    return KRW / 1245.89;
-}
-
-int toJPY(double KRW){
-    return KRW / 10.1949;
-}
+double toUSD(double KRW);
+double toEUR(double KRW);
+int toJPY(double KRW);
 
 void main(){
     int K = 0;
@@ -19,4 +14,14 @@ void main(){
     printf("USD : %.2lf\n", toUSD(K));
     printf("EUR : %.2lf\n", toEUR(K));
     printf("JPY : %d\n", toJPY(K));
+}
+
+double toUSD(double KRW){
+    return KRW / _USD;
+}
+double toEUR(double KRW){
+    return KRW / _EUR;
+}
+int toJPY(double KRW){
+    return KRW / _JPY;
 }
