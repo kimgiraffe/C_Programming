d0b3664c4ddc8788a89675d16c97823bba21ad42
kimgiraffe <kimgiraffe@naver.com>
2022-12-29 15:24:42 +0900

    함수

--- 221229/ex4.c
+++ 221229/ex4.c
@@ -10,13 +10,7 @@ void main(){
 }
 
 int Fib(int n){
-    if(n == 0){
-        return 0;
-    }
-    else if(n == 1){
-        return 1;
-    }
-    else{
-        return Fib(n-1) + Fib(n-2);
-    }
+    if(n == 0) return 0;
+    else if(n == 1) return 1;
+    else return Fib(n-1) + Fib(n-2);
 }
