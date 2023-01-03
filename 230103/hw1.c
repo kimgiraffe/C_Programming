f346f102f2995f71a0ad49295e7a08765e9c3fde
kimgiraffe <kimgiraffe@naver.com>
2023-01-03 22:12:55 +0900

    I/O, String

--- 230103/hw1.c
+++ 230103/hw1.c
@@ -6,11 +6,17 @@ void main(void) {
 	fgets(ID, sizeof(ID), stdin);
 	while(getchar() != '\n');  //입력 overflow 발생을 고려하여 버퍼를 비운다.
 	//입력에＇\n＇이 포함된 경우 ＇\0＇으로 바꾼다.
+	for(int i = 0; i < 8; i++){
+		if(ID[i] == '\n') ID[i] = '\0';
+	}
     
 	fputs("이름을 입력 하세요: ", stdout);
 	fgets(name, sizeof(name), stdin);
 	while(getchar() != '\n');  //입력 overflow 발생을 고려하여 버퍼를 비운다.
 	//입력에＇\n＇이 포함된 경우 ＇\0＇으로 바꾼다.
+	for(int i = 0; i < 10; i++){
+		if(name[i] == '\n') name[i] = '\0';
+	}
     
 	printf("학번: %s\n", ID);
 	printf("이름: %s\n", name);
