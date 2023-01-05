553d832343173cbfce695f5c6c8cefc17c507798
kimgiraffe <kimgiraffe@naver.com>
2023-01-05 14:20:07 +0900

    Structure

--- 230105/ex1.c
+++ 230105/ex1.c
@@ -1,43 +1,56 @@
 #include <stdio.h>
 #include <string.h>
-
 typedef struct {
-    char name[20];
-    char mail[20];
-    int mobile;
-    char major[5];
-    int id;
-    float cgpa;
-} Person;
+	char name[20];
+	char mail[20];
+	int mobile;
+} PROFESSOR;
+typedef struct {
+	char name[20];
+	char major[10];
+	int ID;
+	float cgpa;
+} STUDENT;
+typedef struct {
+	char type;
+	union {
+		PROFESSOR prof;
+		STUDENT stu;
+	}u;
+}PERSON;
 
-void printPerson(Person p);
+void printPerson(PERSON p);
 
 void main() {
-    Person person1 = {"james" , "james@hanmail.net", 6055726, "", 0, 0.0};
-    Person person2 = {"david" , "", 0, "cs", 20010123, 0.550000};
+	PERSON person1, person2;
+	person1.type = 1;
+	strcpy(person1.u.prof.name, "james");
+	strcpy(person1.u.prof.mail, "james@hanmail.net");
+	person1.u.prof.mobile = 027063456;
 
-    printPerson(person1);
-    printPerson(person2);
+	person2.type = 2;
+	strcpy(person2.u.stu.name, "david");
+	strcpy(person2.u.stu.major, "cs");
+	person2.u.stu.ID = 20010123;
+	person2.u.stu.cgpa = 0.55f;
+
+	printPerson(person1);
+	printPerson(person2);
 }
 
-void printPerson(Person p){
-    printf("-------------------------\n");
-    if(strcmp(p.name, "") != 0){
-        printf("name: %s\n", p.name);
-    }
-    if(strcmp(p.mail, "") != 0){
-        printf("mail: %s\n", p.mail);
-    }
-    if(p.mobile != 0){
-        printf("mobile: %d\n", p.mobile);
-    }
-    if(strcmp(p.major, "") != 0){
-        printf("major: %s\n", p.major);
-    }
-    if(p.id != 0){
-        printf("id: %d\n", p.id);
-    }
-    if(p.cgpa != 0.0){
-        printf("cgpa: %f\n", p.cgpa);
-    }
+void printPerson(PERSON p) {
+
+	if (p.type == 1) {
+		printf("-------------------------\n");
+		printf("name: %s\n", p.u.prof.name);
+		printf("mail: %s\n", p.u.prof.mail);
+		printf("mobile: %d\n", p.u.prof.mobile);
+	}
+	else {
+		printf("-------------------------\n");
+		printf("name: %s\n", p.u.stu.name);
+		printf("major: %s\n", p.u.stu.major);
+		printf("id: %d\n", p.u.stu.ID);
+		printf("cgpa: %f\n", p.u.stu.cgpa);
+	}
 }
