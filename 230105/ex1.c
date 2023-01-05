eb3102533650553d987d8fa793c0062f22be77d6
kimgiraffe <kimgiraffe@naver.com>
2023-01-05 14:46:31 +0900

    Structure

--- 230105/ex1.c
+++ 230105/ex1.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+
 typedef struct {
 	char name[20];
 	char mail[20];
 	int mobile;
 } PROFESSOR;
+
 typedef struct {
 	char name[20];
 	char major[10];
 	int ID;
 	float cgpa;
 } STUDENT;
+
 typedef struct {
 	char type;
 	union {
 		PROFESSOR prof;
 		STUDENT stu;
-	}u;
+	}status;
 }PERSON;
 
 void printPerson(PERSON p);
@@ -24,33 +27,32 @@ void printPerson(PERSON p);
 void main() {
 	PERSON person1, person2;
 	person1.type = 1;
-	strcpy(person1.u.prof.name, "james");
-	strcpy(person1.u.prof.mail, "james@hanmail.net");
-	person1.u.prof.mobile = 027063456;
+	strcpy(person1.status.prof.name, "james");
+	strcpy(person1.status.prof.mail, "james@hanmail.net");
+	person1.status.prof.mobile = 027063456;
 
 	person2.type = 2;
-	strcpy(person2.u.stu.name, "david");
-	strcpy(person2.u.stu.major, "cs");
-	person2.u.stu.ID = 20010123;
-	person2.u.stu.cgpa = 0.55f;
+	strcpy(person2.status.stu.name, "david");
+	strcpy(person2.status.stu.major, "cs");
+	person2.status.stu.ID = 20010123;
+	person2.status.stu.cgpa = 0.55f;
 
 	printPerson(person1);
 	printPerson(person2);
 }
 
 void printPerson(PERSON p) {
-
 	if (p.type == 1) {
 		printf("-------------------------\n");
-		printf("name: %s\n", p.u.prof.name);
-		printf("mail: %s\n", p.u.prof.mail);
-		printf("mobile: %d\n", p.u.prof.mobile);
+		printf("name: %s\n", p.status.prof.name);
+		printf("mail: %s\n", p.status.prof.mail);
+		printf("mobile: %d\n", p.status.prof.mobile);
 	}
 	else {
 		printf("-------------------------\n");
-		printf("name: %s\n", p.u.stu.name);
-		printf("major: %s\n", p.u.stu.major);
-		printf("id: %d\n", p.u.stu.ID);
-		printf("cgpa: %f\n", p.u.stu.cgpa);
+		printf("name: %s\n", p.status.stu.name);
+		printf("major: %s\n", p.status.stu.major);
+		printf("id: %d\n", p.status.stu.ID);
+		printf("cgpa: %f\n", p.status.stu.cgpa);
 	}
 }
